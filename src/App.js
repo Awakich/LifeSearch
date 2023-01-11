3bcc46025d8df01bb6ee82bbdb9a7d5ad4e409c5
Artem <artem.bqpower@gmail.com>
2023-01-12 02:30:03 +0300

    add books api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import Header from "../src/components/Header";
+import Advantage from "../src/components/Advantage";
+import Books from "../src/components/Books";
+import Footer from "../src/components/Footer";
+import { useState } from "react";
+import { useEffect } from "react";
+
 function App() {
-  return <div>hi</div>;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const GetBooks = async () => {
+      const res = await fetch("https://api.itbook.store/1.0/new").then((res) =>
+        res.json()
+      );
+      setBooks(res.books);
+    };
+    GetBooks();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Advantage />
+      <Books books={books} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,7 +1,20 @@
-const Books = () => {
-  return (
-    <div>Books</div>
-  )
-}
+// title, subtitle, isbn13, image, price
+import Book from "../components/Book";
 
-export default Books
+const Books = ({ books }) => {
+  return (
+    <div className="flex items-center flex-wrap gap-5 justify-center">
+      {books.map(({ subtitle, title, price, isbn13 }) => (
+        <Book
+          description={subtitle}
+          title={title}
+          price={price}
+          id={isbn13}
+          key={isbn13}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Books;
